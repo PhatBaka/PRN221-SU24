6744e1352e26ecce6160ec6633c6e870e23f5444
huyqu <huyquac@gmail.com>
2024-07-17 21:33:31 +0700

    add crud promotion

---
+++ UI/Pages/Promotions/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BusinessObjects;
+using DataAccessObjects;
+using System.Diagnostics;
+
+namespace UI.Pages.Promotions
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DataAccessObjects.AppDBContext _context;
+
+        public CreateModel(DataAccessObjects.AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Promotion Promotion { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (_context.Promotions == null || Promotion == null)
+            {
+                return Page();
+            }
+
+            _context.Promotions.Add(Promotion);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
