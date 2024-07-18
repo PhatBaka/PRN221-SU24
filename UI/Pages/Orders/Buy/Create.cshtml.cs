b22ce074662ed3215595baf752d25319475f9c93
PhatBaka <nguyenquyphat2711@gmail.com>
2024-07-18 21:55:52 +0700

    add metal to cart done

--- UI/Pages/Orders/Buy/Create.cshtml.cs
+++ UI/Pages/Orders/Buy/Create.cshtml.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.Interfaces;
+using UI.Helper;
+using UI.Payload.JewelryPayload;
 
 namespace UI.Pages.Orders.Buy
 {
@@ -23,11 +25,75 @@ namespace UI.Pages.Orders.Buy
         }
 
         public IList<MetalResponse>? Metals { get; set; }
-
+        public IList<MetalCart> MetalCarts { get; set; }
 
         public void OnGet()
         {
-            Metals = _metalService.GetPrices();
+            LoadData();
+           
         }
+
+		[BindProperties]
+		public class CartItem
+		{
+			public GetJewelryRequest Jewelry { get; set; }
+			public int Quantity { get; set; }
+			public decimal UnitPrice { get; set; }
+			public decimal DiscountValue { get; set; }
+			public decimal GetTotalPrice() => UnitPrice * Quantity;
+			public decimal GetDiscountPrice() => GetTotalPrice() * DiscountValue / 100;
+			public decimal GetFinalPrice() => GetTotalPrice() - GetDiscountPrice();
+		}
+
+        [BindProperties]
+		public class MetalCart
+		{
+			public MetalResponse? Item { get; set; }
+			public decimal Weight { get; set; }
+		}
+
+        public IActionResult OnPostRemoveMetalFromCart(decimal weight)
+        {
+            LoadData();
+
+            return Page();
+        }
+
+        private void LoadData()
+        {
+            LoadMetalCart();
+            LoadPrice();
+        }
+
+        public IActionResult OnPostAddMetalToCart(string metal)
+        {
+            LoadData();
+            MetalCarts ??= new List<MetalCart>();
+
+            if (MetalCarts != null && MetalCarts.Count > 0)
+            {
+                foreach (var item in MetalCarts)
+                {
+                    // check metal already in cart
+                    if (item.Item.Metal.Equals(metal))
+                    {
+                        return Page();
+                    }
+                }
+            }
+
+            MetalCart metalCart = new()
+            {
+                Item = Metals.FirstOrDefault(x => x.Metal.Equals(metal))
+            };
+
+            MetalCarts.Add(metalCart);
+            HttpContext.Session.SetObjectAsJson("METALCARTS", MetalCarts);
+
+            return Page();
+        }
+
+        private void LoadMetalCart() => MetalCarts = HttpContext.Session.GetObjectFromJson<IList<MetalCart>>("METALCARTS");
+        private void LoadPrice() => Metals = _metalService.GetPrices();
     }
 }
