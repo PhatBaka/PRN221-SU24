370e8314f5e7b09d5ecbc7e3d73a53895934f08e
PhatBaka <nguyenquyphat2711@gmail.com>
2024-07-22 19:15:37 +0700

    temp

---
+++ DataAccessObjects/Interfaces/IOrderDAO.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects.Interfaces
+{
+    public interface IOrderDAO : IGenericDAO<Order>
+    {
+        public Order GetOrderById(int id);
+    }
+}
